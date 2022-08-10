9769d0579b68f8f2031ecebc0e269e7f3255cf01
Support <Support@WEMA-TRAINING-S>
2022-08-10 00:19:27 -0700

    Add project files.

---
+++ INEC2 ASSIGNMENT/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace INEC2_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> votersName = new SortedSet<string>();
+
+            votersName.Add("joy");
+            votersName.Add("victor");
+            votersName.Add("joy");
+            votersName.Add("victor");
+            votersName.Add("peace");
+            votersName.Add("yinka");
+            votersName.Add("joy");
+            votersName.Add("tola");
+            votersName.Add("femi");
+            votersName.Add("precious");
+            votersName.Add("joy");
+            votersName.Add("tosin");
+            
+            
+            
+            
+            
+            
+            Console.WriteLine("enter voter name");
+            string newName = Console.ReadLine();
+
+
+            if (votersName.Contains(newName))
+            {
+                Console.WriteLine("Voter already exists");
+            }
+            else
+            {
+                votersName.Add(newName);
+                Console.WriteLine("User successfully added");
+
+            }
+        }
+    }
+}
